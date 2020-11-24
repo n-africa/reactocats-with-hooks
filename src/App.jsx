2e346709ebe808010d6745dc2de455c2b9373fe9
Naima Africa Francis <naima.africa@gmail.com>
2020-11-24 00:42:31 -0500

    Created Header function and imported to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
 
 function App() {
   return (
     <>
+      <Header />
       <Footer />
     </>
   )
